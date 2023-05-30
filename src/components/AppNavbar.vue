da52e3988d5ea82ba563722119f3c4022e6e6f5a
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-05-30 15:45:33 +0200

    feat: navbar

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,15 +1,63 @@
 <script>
     export default {
-        name: "AppNavbar"
+        name: "AppNavbar",
+        data() {
+            return {
+                navBar: [
+                    {label: "HOME", link: "#"},
+                    {label: "ABOUT", link: "#"},
+                    {label: "SERVICES", link: "#"},
+                    {label: "WORK", link: "#"},
+                    {label: "ARTICLES", link: "#"},
+                ]    
+            }
+        }
     }
 </script>
 
 <template>
-    <div class="container">
-        <h1>Test</h1>
-    </div>
+    <section class="container">
+        <div class="row">
+            <div class="p-4 d-flex justify-content-between align-items-center">
+                <div>
+                    <a href="#"><img src="../assets/images-construction/imagesPush/construction_logo.png" alt="logo"></a>
+                </div>
+                <div>
+                    <ul class="d-flex align-items-center gap-4 list-style-none list-inline">
+                        <li v-for="item in navBar">
+                            <a :href="item.link" class="link-unstyled">{{ item.label }}</a>
+                        </li>
+                        <button><a>GET QUOTE</a></button>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use '../styles/variables.scss' as *;
+
+    a {
+        color: black;
+        text-decoration: none;
+        font-weight: 400;
+    }
+    a:hover {
+        color: $yellow;
+        text-decoration: none;
+    }
+
+    button {
+        background-color: $yellow;
+        color: black;
+        border: none;
+        width: 10rem;
+        padding: 0.5rem;
+    }
+    button:hover {
+        background-color: black;
+        color: white;
+    }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import './styles/general.scss'
-import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/general.scss'
+import App from './App.vue'
 
 createApp(App).mount('#app')
