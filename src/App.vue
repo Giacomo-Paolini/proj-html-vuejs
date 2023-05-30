9c3795931e033fb34b75c0e693ff693d7226d43b
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-05-30 12:37:45 +0200

    feat: scaffolding vite

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  import AppNavbar from './components/AppNavbar.vue'
+  
+  export default {
+    name: "App",
+    components: {
+      AppNavbar
+    }
+  }
+</script>
+
+<template>
+  <header>
+    <AppNavbar />
+  </header>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,15 @@
+<script>
+    export default {
+        name: "AppNavbar"
+    }
+</script>
+
+<template>
+    <div class="container">
+        <h1>Test</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
