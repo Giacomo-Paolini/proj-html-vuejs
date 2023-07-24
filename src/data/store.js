687e613fa3d5a6ae7227517a1967c98504b35456
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-07-24 22:16:01 +0200

    public img

--- src/data/store.js
+++ src/data/store.js
@@ -21,9 +21,9 @@ export const store = reactive({
         {title: "INTERNATIONAL OFFICES", num: "19", icon: `<i class="fa-solid fa-globe"></i>`},
     ],
     cardSection2: [
-        {image: '../../src/assets/images-construction/imagesPush/project1-featured-294276386-600x600.jpg'},
-        {image: '../../src/assets/images-construction/imagesPush/project2-featured-15013609-600x600.jpg'},
-        {image: '../../src/assets/images-construction/imagesPush/project3-featured-189023420-600x600.jpg'},
+        {image: '/images-construction/imagesPush/project1-featured-294276386-600x600.jpg'},
+        {image: '/images-construction/imagesPush/project2-featured-15013609-600x600.jpg'},
+        {image: '/images-construction/imagesPush/project3-featured-189023420-600x600.jpg'},
     ],
     iconItems: [
         {style: "my-i-home", title: "Great Services", icon: `<i class="fa-solid fa-house"></i>`, description: "Quis autem vel eum iure reprederit qui in ea volupta e velit esse quam nihil molestiae consequatur."},
@@ -32,19 +32,19 @@ export const store = reactive({
         {style: "my-i-idea", title: "Creative Solutions", icon: `<i class="fa-regular fa-lightbulb"></i>`, description: "Quis autem vel eum iure reprederit qui in ea volupta e velit esse quam nihil molestiae consequatur."},
     ],
     stories: [
-        {name: "HARRY SMITH - NEW HOME OWNER", story: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe error asperiores dignissimos! Explicabo alias illo delectus beatae, necessitatibus reiciendis. Sequi ipsum reiciendis quod? Tempora et nisi iste. Fuga, nisi totam!", image: "../src/assets/images-construction/imagesPush/testimonial.jpg"},
-        {name: "FRANCO - NEW HOME OWNER", story: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe error asperiores dignissimos! Explicabo alias illo delectus beatae, necessitatibus reiciendis. Sequi ipsum reiciendis quod? Tempora et nisi iste. Fuga, nisi totam!", image: "../src/assets/images-construction/imagesPush/testimonial2.jpg"},
+        {name: "HARRY SMITH - NEW HOME OWNER", story: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe error asperiores dignissimos! Explicabo alias illo delectus beatae, necessitatibus reiciendis. Sequi ipsum reiciendis quod? Tempora et nisi iste. Fuga, nisi totam!", image: "/images-construction/imagesPush/testimonial.jpg"},
+        {name: "FRANCO - NEW HOME OWNER", story: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe error asperiores dignissimos! Explicabo alias illo delectus beatae, necessitatibus reiciendis. Sequi ipsum reiciendis quod? Tempora et nisi iste. Fuga, nisi totam!", image: "/images-construction/imagesPush/testimonial2.jpg"},
     ],
     articles: [
-        {title: "Redeveloping Florida's Remote Southern Coast", date: "December 7th 2015", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe error asperiores dignissimos! Explicabo alias illo delectus beatae, necessitatibus reiciendis. Sequi ipsum reiciendis quod? Tempora et nisi iste. Fuga, nisi totam!", image: '../../src/assets/images-construction/imagesPush/blog-post-134132600-600x362.jpg'},
-        {title: "How We Manage Large Construction Projects", date: "December 7th 2015", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe error asperiores dignissimos! Explicabo alias illo delectus beatae, necessitatibus reiciendis. Sequi ipsum reiciendis quod? Tempora et nisi iste. Fuga, nisi totam!", image: '../../src/assets/images-construction/imagesPush/blog-post-92486644-600x362.jpg'},
-        {title: "Future proofing a modern home", date: "December 6th 2015", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe error asperiores dignissimos! Explicabo alias illo delectus beatae, necessitatibus reiciendis. Sequi ipsum reiciendis quod? Tempora et nisi iste. Fuga, nisi totam!", image: '../../src/assets/images-construction/imagesPush/blog-post-332773904-600x362.jpg'},
+        {title: "Redeveloping Florida's Remote Southern Coast", date: "December 7th 2015", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe error asperiores dignissimos! Explicabo alias illo delectus beatae, necessitatibus reiciendis. Sequi ipsum reiciendis quod? Tempora et nisi iste. Fuga, nisi totam!", image: '/images-construction/imagesPush/blog-post-134132600-600x362.jpg'},
+        {title: "How We Manage Large Construction Projects", date: "December 7th 2015", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe error asperiores dignissimos! Explicabo alias illo delectus beatae, necessitatibus reiciendis. Sequi ipsum reiciendis quod? Tempora et nisi iste. Fuga, nisi totam!", image: '/images-construction/imagesPush/blog-post-92486644-600x362.jpg'},
+        {title: "Future proofing a modern home", date: "December 6th 2015", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe error asperiores dignissimos! Explicabo alias illo delectus beatae, necessitatibus reiciendis. Sequi ipsum reiciendis quod? Tempora et nisi iste. Fuga, nisi totam!", image: '/images-construction/imagesPush/blog-post-332773904-600x362.jpg'},
     ],
     partners: [
-        {logo: "../src/assets/images-construction/imagesPush/home-logo11-219096700-320x202.png" },
-        {logo: "../src/assets/images-construction/imagesPush/home-logo10-219096700-320x202.png" },
-        {logo: "../src/assets/images-construction/imagesPush/home-logo2-219096700-320x202.png" },
-        {logo: "../src/assets/images-construction/imagesPush/home-logo1-219096700-320x202.png" },
-        {logo: "../src/assets/images-construction/imagesPush/home-logo9-219096700-320x202.png"},
+        {logo: "/images-construction/imagesPush/home-logo11-219096700-320x202.png" },
+        {logo: "/images-construction/imagesPush/home-logo10-219096700-320x202.png" },
+        {logo: "/images-construction/imagesPush/home-logo2-219096700-320x202.png" },
+        {logo: "/images-construction/imagesPush/home-logo1-219096700-320x202.png" },
+        {logo: "/images-construction/imagesPush/home-logo9-219096700-320x202.png"},
     ]
 });
